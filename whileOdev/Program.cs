8b493ce62e935fb0bf5e84a37a9c319e595be581
onur engun <onurengun@outlook.com>
2021-01-20 03:02:17 +0300

    Add project files.

---
+++ whileOdev/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            int sayac = 0;
+            int toplam=0;
+            while (sayi <=100)
+            {
+                if (sayi % 4==0)
+                {
+                    sayac++;
+                    toplam += sayi;
+                }
+                sayi++;
+            }
+            Console.WriteLine("{0} adet sayı vardır. Toplamları: {1}",sayac,toplam);
+        }
+    }
+}
